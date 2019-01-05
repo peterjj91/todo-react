0d92bb98ccd3ea3faae2396a16e2009818bfcef7
peterjj91 <peterjj91@gmail.com>
2019-01-05 23:06:15 +0200

    Logic: one component - one file.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const TodoList = () => {
-    return (
-        <ul>
-            <li>Learn React</li>
-            <li>Build Awesome App</li>
-        </ul>
-    )
-};
-
-const AppHeader = () => {
-    return <h1>My Todo list</h1>
-}
-
-const SearchPanel = () => {
-    return <input placeholder="search" />
-}
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
 
 const App = () => {
-    return (
-        <div>
-            <AppHeader />
-            <SearchPanel />
-            <TodoList />
-        </div>
-    )
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
