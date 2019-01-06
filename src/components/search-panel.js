865a42d8bcc18e153728ca3e51dd726b3a4541d4
peterjj91 <peterjj91@gmail.com>
2019-01-06 17:23:37 +0200

    Add style for search panel and fix search panel component.

--- src/components/search-panel.js
+++ src/components/search-panel.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
+import './search-panel.css';
+
 const SearchPanel = () => {
   const searchText = 'Type here to search';
   const searchStyle = {
-      fontSize: '21px'
+    fontSize: '21px',
+    marginRight: '10px'
   };
   return <input 
       style={searchStyle}
-      placeholder={searchText} />;
+      placeholder={searchText} 
+      className="form-control"/>;
 }
 
 export default SearchPanel;
