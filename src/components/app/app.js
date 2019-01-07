744be27447df1aac39000d67055fca59f62d223b
peterjj91 <peterjj91@gmail.com>
2019-01-07 15:11:28 +0200

    Delete item list with the help of new array without delete original todoData.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
@@ -7,24 +7,44 @@ import ItemStatusFilter from '../item-status-filter';
 
 import './app.css';
 
-const App = () => {
-  const todoData = [
-    { label: 'Drink coffee', important: false, id: 1 },
-    { label: 'Make awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 }
-  ]
-  return (
-    <div className="todo-app">
-      <AppHeader />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
-      </div>
-      <TodoList 
-        todos={todoData} 
-        onDeleted={ (id) => console.log('del', id) }/>
-    </div>
-  )
-}
+export default class App extends Component {
+  state = {
+    todoData: [
+      { label: 'Drink coffee', important: false, id: 1 },
+      { label: 'Make awesome App', important: true, id: 2 },
+      { label: 'Have a lunch', important: false, id: 3 }
+    ]
+  };
 
-export default App;
+  deleteItem = (id) => {
+    this.setState(({todoData}) => {
+      const idx = todoData.findIndex((el) => el.id === id);
+
+      // Cannot change original todoData
+      // Our new array without element
+      const newArray = [
+        ...todoData.slice(0, idx), 
+        ...todoData.slice(idx + 1)
+      ]
+
+      return {
+        todoData: newArray // set new array 
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div className="todo-app">
+        <AppHeader />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+        <TodoList 
+          todos={this.state.todoData} 
+          onDeleted={ this.deleteItem }/>
+      </div>
+    )
+  }
+};
