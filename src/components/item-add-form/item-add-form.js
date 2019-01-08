58dcd4df69c9e872634697767f28d692606e1923
peterjj91 <peterjj91@gmail.com>
2019-01-08 09:53:41 +0200

    Add new task with the help of form.

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,23 +3,43 @@ import React, { Component } from 'react';
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
+  state = {
+    label: ''
+  }
+
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onItemAdded(this.state.label);
+    this.setState({
+      label: ''
+    })
+  }
+
   render() {
     const addBtnStyle = {
       whiteSpace: 'nowrap'
     };
 
     return (
-      <div className="item-add-form d-flex">
+      <form className="item-add-form d-flex"
+            onSubmit={this.onSubmit}>
         <input type="text"
                className="form-control"
-               placeholder="What needs to be done" />
+               onChange={this.onLabelChange}
+               placeholder="What needs to be done"
+               value={this.state.label} />
         <button
           className="btn btn-outline-secondary"
-          style={addBtnStyle}
-          onClick={() => this.props.onItemAdded("Hello")}>
+          style={addBtnStyle}>
           Add Item
         </button>
-      </div>
+      </form>
     );
   }
 }
